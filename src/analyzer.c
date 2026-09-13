a9959e022dee80b3bbf39b998a63cf3b17cc56c6
Xaabibas <tumuyhabirov@yandex.ru>
2026-09-13 13:46:23 +0300

    fix analyzer: check boundaries

--- src/analyzer.c
+++ src/analyzer.c
@@ -4,7 +4,7 @@
 #include "token.h"
 
 
-static int check_str_len(char **str)
+static int exactly_one_argument(char **str)
 {
 	if (!*str) {
 		return 0;
@@ -23,24 +23,24 @@ int analyze(int size, token *tokens)
 		switch (tokens[i].type) {
 			case IN:
 				if (in) {
-					printf("Error: double in\n");
+					printf("Error: multiple input redirection\n");
 					return 0;
 				}
 				in = 1;
-				if (!check_str_len(tokens[++i].str)) {
-					printf("Error: you can specify only one file\n");
+				if (i + 1 >= size || !exactly_one_argument(tokens[++i].str)) {
+					printf("Error: one input file expected\n");
 					return 0;
 				}
 				break;
 			case OUT:
 			case APPEND:
 				if (out) {
-					printf("Error: double out\n");
+					printf("Error: multiple output redirection\n");
 					return 0;
 				}
 				out = 1;
-				if (!check_str_len(tokens[++i].str)) {
-					printf("Error: you can specify only one file\n");
+				if (i + 1 >= size || !exactly_one_argument(tokens[++i].str)) {
+					printf("Error: one output file expected\n");
 					return 0;
 				}
 				break;
